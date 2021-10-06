c8ca14b4109835450c544440e66ddb5a16af520d
Moises Morales <moisesmorales1995@gmail.com>
2021-10-06 15:16:15 -0500

    Little miniature of the way SwiftUI Works

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,22 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emoji = ["🎃","👻","👽","👺"]
+    
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, Clase!")
-                .foregroundColor(.orange)
+        
+        HStack{
+            CardView(content: "🎃")
+            CardView(content: "👻")
+            CardView(content: "👽")
+            CardView(content: "👺")
         }
         .padding(.horizontal)
         .foregroundColor(.red)
-        
 
+    }
+}
+
+struct CardView: View{
+    
+    var content : String
+    @State var isFaceUp : Bool = true
+    
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
