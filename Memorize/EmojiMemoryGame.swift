e71d627e28a718cd8f2cc3f19cd078153383e9db
Moises Morales <moisesmorales1995@gmail.com>
2021-10-16 15:06:08 -0500

    The MVVM architecture is fully applied to Memorize

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -13,7 +13,7 @@ class EmojiMemoryGame : ObservableObject {
     static let emojis = ["🎃","👻","😈","👿","💀","👺","🧟‍♂️","🧛🏻‍♂️","🧙🏻‍♂️","🕷","🦇","🕸","🧛🏻‍♀️","🧟‍♀️","🧞‍♀️","🧚🏻‍♀️"]
     
     static func createMemoryGame()->MemoryGame<String>{
-        MemoryGame<String>(numberOfPairsOfCards: 4){ pairIndex in
+        MemoryGame<String>(numberOfPairsOfCards: 8){ pairIndex in
             emojis[pairIndex]
         }
     }
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,22 +7,34 @@
 
 import Foundation
 
-struct MemoryGame<CardContent>{
+struct MemoryGame<CardContent> where CardContent : Equatable{
     
     private(set) var cards : Array<Card>
     
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
+    
     mutating func choose(_ card:Card){
-        let chosenIndex = index(of:card)
-        cards[chosenIndex].isFaceUp.toggle()
-        print("\(cards)")
-    }
-    func index(of card: Card)->Int{
-        for index in 0..<cards.count{
-            if cards[index].id == card.id{
-                return index
+        //if let chosenIndex = index(of:card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id  }),
+            !cards[chosenIndex].isFaceUp,
+            !cards[chosenIndex].isMatched
+        {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            }else{
+                for index in 0..<cards.count{
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
             }
+            
+            cards[chosenIndex].isFaceUp.toggle()
         }
-        return 0 //Bogus!
+        //print("\(cards)")
     }
     
     init (numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent ){
@@ -36,7 +48,7 @@ struct MemoryGame<CardContent>{
     }
     
     struct Card : Identifiable{
-        var isFaceUp : Bool = true
+        var isFaceUp : Bool = false
         var isMatched : Bool = false
         var content: CardContent
         var id: Int
